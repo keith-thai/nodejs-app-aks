830960b0f1b2e795cf0dddd0d104b59e94610977
keiththai18003 <keiththai18003@gmail.com>
2020-10-07 19:56:19 -0500

    5th commit from VS Code + Delete nodejs-app-service + nodejs-app-deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use('*', (req, res, next) => {
-  res.send('<h1>Hello there ! from local VSCode wassup</h1>');
+  res.send('<h1>Hello there 5th timme after delete  nodejs-app-deployment and nodejs-app-service! from local VSCode wassup</h1>');
 });
 
 app.listen(4000, () => {
